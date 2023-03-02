8b060c687854f9006d06d024782d72bb4de05f0d
jrodg85 <63821662+jrodg85@users.noreply.github.com>
2023-03-02 13:43:35 +0100

    generando modulo

---
+++ jrodg85mod1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  2 08:32:13 2023
+
+@author: jrodg85
+"""
+
+def estadistica_por_columna(matriz,columna=None):
+    return True
+
+    
+    
+def estadistica_por_fila(matriz,fila):
+    return True
+    
+    
+    
+    
